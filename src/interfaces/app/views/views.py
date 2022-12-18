d7c005f0e6dcb8bb7d513f8c8bf77432f67df43e
DM880 <denys.mauro@gmail.com>
2022-12-18 07:18:51 +0000

    geo location news test

--- src/interfaces/app/views/views.py
+++ src/interfaces/app/views/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect, reverse
 from pygooglenews import GoogleNews
 
-gn = GoogleNews()
-s = gn.search('ukraine')
+# gn = GoogleNews()
+# s = gn.search('ukraine')
 
-for entry in s["entries"]:
-    print(entry["title"])
+# for entry in s["entries"]:
+#     print(entry["title"])
 
 
 def home(request):
 
-    return render(request, "home.html")
+    gn = GoogleNews(country="NZ")
+
+    s = gn.search('new')
+    latest = [()]
+
+    for entry in s["entries"]:
+        latest.append(entry['title'])
+        print(entry["title"])
+
+
+
+    return render(request, "home.html", {'latest':latest})
