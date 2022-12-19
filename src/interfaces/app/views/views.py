576f271c31b7a50ca905e4b1670167da09ee8074
DM880 <denys.mauro@gmail.com>
2022-12-19 08:02:35 +0000

    if inputs or input empty logic to refactor

--- src/interfaces/app/views/views.py
+++ src/interfaces/app/views/views.py
@@ -32,17 +32,63 @@ def search_topic(request):
         geo = request.POST.get('location')
         time = request.POST.get('time')
 
+        if not topic or None and geo:
+            gn = GoogleNews(country=geo)
+            s = gn.top_news()
+
+            results = []
+            count = 0
+
+            for entry in s["entries"]:
+                if count < 12:
+                    results.append((entry['title'], entry['link']))
+                    count += 1
+            return render(request, "search_results.html", {'results':results})
+
+        elif not geo or None and topic:
+            gn = GoogleNews()
+            s = gn.search(topic)
+
+            results = []
+            count = 0
+
+            for entry in s["entries"]:
+                if count < 12:
+                    results.append((entry['title'], entry['link']))
+                    count += 1
+            return render(request, "search_results.html", {'results':results})
+
+        elif topic and geo and not time or None:
+
+            gn = GoogleNews(country=geo)
+            s = gn.search(topic)
+
+            results = []
+            count = 0
+
+            for entry in s["entries"]:
+                if count < 12:
+                    results.append((entry['title'], entry['link']))
+                    count += 1
+            return render(request, "search_results.html", {'results':results})
+
+
+        elif time and topic and geo:
+            gn = GoogleNews()
+            s = gn.search(topic, when=time)
+            count = 0
+
+            for entry in s["entries"]:
+                if count < 12:
+                    results.append((entry['title'], entry['link']))
+                    count += 1
+
+            return render(request, "search_results.html", {'results':results})
+
         return redirect(reverse('search_results', args=(topic,geo,time)))
 
 
 def search_results(request, topic, geo, time):
-
-    if not topic or None:
-        topic = ''
-    if not geo or None:
-        geo = ''
-    if not time or None:
-        time = ''
 
     gn = GoogleNews(country=geo)
     s = gn.search(topic, when=time)
