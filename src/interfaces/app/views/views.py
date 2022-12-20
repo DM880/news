b6ced9b3d69b66ccd3cc5d9ca9b792451541bcd9
DM880 <denys.mauro@gmail.com>
2022-12-20 09:38:14 +0000

    search by default topics

--- src/interfaces/app/views/views.py
+++ src/interfaces/app/views/views.py
@@ -21,26 +21,27 @@ def search_topic(request):
 
     if request.method == 'POST':
 
-        topic = request.POST.get('topic')
+        keyword = request.POST.get('keyword')
         geo = request.POST.get('location')
         time = request.POST.get('time')
         language = request.POST.get('language')
+        topic = request.POST.get('topic')
 
-        if not topic or None and geo:
+        if not keyword or None and geo:
             gn = GoogleNews(lang=language, country=geo)
             s = gn.top_news()
 
-        elif not geo or None and topic:
+        elif not geo or None and keyword:
             gn = GoogleNews(lang=language)
-            s = gn.search(topic)
+            s = gn.search(keyword)
 
-        elif topic and geo and not time or None:
+        elif keyword and geo and not time or None:
             gn = GoogleNews(lang=language, country=geo)
-            s = gn.search(topic)
+            s = gn.search(keyword)
 
         else:
             gn = GoogleNews(lang=language, country=geo)
-            s = gn.search(topic, when=time)
+            s = gn.search(keyword, when=time)
 
         results = []
         count = 0
